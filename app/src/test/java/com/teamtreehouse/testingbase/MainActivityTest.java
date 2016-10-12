0f11cb6e60ec4903ad74397fa3197ebe67e29c50
Aldo Kelvianto <aldokelvianto@yahoo.com>
2016-10-12 09:13:26 +0700

    add roboelectric support

--- app/src/test/java/com/teamtreehouse/testingbase/MainActivityTest.java
+++ app/src/test/java/com/teamtreehouse/testingbase/MainActivityTest.java
@@ -4,21 +4,31 @@ import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.Shadows;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.view.inputmethod.EditorInfo;
 
 /**
  * Created by aldo on 10/6/16.
  */
-
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
 public class MainActivityTest {
 
     MainActivity activity;
 
     @Before
     public void setUp() {
-        activity = new MainActivity();
-        activity.onCreate(null);
+        activity = Robolectric.setupActivity(MainActivity.class);
     }
 
     @Test
@@ -37,6 +47,38 @@ public class MainActivityTest {
         // Assert
         String actuaString = activity.editText.getText().toString();
         Assert.assertEquals(givenString, actuaString);
+    }
+
+    @Test
+    public void spinnerUpdatesBackgroundColor() throws Exception {
+        // Arrange
+        int index = 2;
+        int givenColor = Color.GREEN;
+
+        // Act
+        activity.changeBackgroundColor(givenColor);
+
+        // Assert
+        int actualColor = ((ColorDrawable) activity.linearLayout.getBackground()).getColor();
+        Assert.assertEquals(givenColor, actualColor);
+
+    }
+
+    @Test
+    public void buttonLaunchesOtherActivity() throws Exception {
+        // Arrange
+        Class clazz = OtherActivity.class;
+        Intent expectedIntent = new Intent(activity, clazz);
+
+        // Act
+        activity.launchActivityButton.callOnClick();
+
+        // Assert
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        // Fail, this intent has extra space
+        // Assert.assertEquals(expectedIntent, actualIntent);
+        Assert.assertTrue(expectedIntent.filterEquals(actualIntent));
     }
 
 }
